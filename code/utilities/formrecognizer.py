a8103645555725c64ec632bbad4abbe3aec0001f
Afaq Javed <afaq@renesistech.com>
2023-11-03 16:45:44 +0500

    OCR Integrated

--- code/utilities/formrecognizer.py
+++ code/utilities/formrecognizer.py
@@ -1,7 +1,10 @@
-from azure.core.credentials import AzureKeyCredential
-from azure.ai.formrecognizer import DocumentAnalysisClient
+# from azure.core.credentials import AzureKeyCredential
+# from azure.ai.formrecognizer import DocumentAnalysisClient
 import os
 from dotenv import load_dotenv
+import PyPDF2
+import requests
+from io import BytesIO
 
 class AzureFormRecognizerClient:
     def __init__(self, form_recognizer_endpoint: str = None, form_recognizer_key: str = None):
@@ -16,42 +19,72 @@ class AzureFormRecognizerClient:
 
     def analyze_read(self, formUrl):
 
-        document_analysis_client = DocumentAnalysisClient(
-            endpoint=self.form_recognizer_endpoint, credential=AzureKeyCredential(self.form_recognizer_key)
-        )
-        
-        poller = document_analysis_client.begin_analyze_document_from_url(
-                "prebuilt-layout", formUrl)
-        layout = poller.result()
-
         results = []
-        page_result = ''
-        for p in layout.paragraphs:
-            page_number = p.bounding_regions[0].page_number
-            output_file_id = int((page_number - 1 ) / self.pages_per_embeddings)
 
-            if len(results) < output_file_id + 1:
-                results.append('')
+        try:
+            # Fetch the PDF file from the URL
+            response = requests.get(formUrl)
 
-            if p.role not in self.section_to_exclude:
-                results[output_file_id] += f"{p.content}\n"
+            if response.status_code == 200:
+                # Create a PDF reader object
+                pdf_bytes = BytesIO(response.content)
+                pdf_reader = PyPDF2.PdfFileReader(pdf_bytes)
 
-        for t in layout.tables:
-            page_number = t.bounding_regions[0].page_number
-            output_file_id = int((page_number - 1 ) / self.pages_per_embeddings)
-            
-            if len(results) < output_file_id + 1:
-                results.append('')
-            previous_cell_row=0
-            rowcontent='| '
-            tablecontent = ''
-            for c in t.cells:
-                if c.row_index == previous_cell_row:
-                    rowcontent +=  c.content + " | "
-                else:
-                    tablecontent += rowcontent + "\n"
-                    rowcontent='|'
-                    rowcontent += c.content + " | "
-                    previous_cell_row += 1
-            results[output_file_id] += f"{tablecontent}|"
+                # Initialize an empty string to store the extracted text
+                text = ""
+
+                # Iterate through each page in the PDF
+                for page_num in range(pdf_reader.getNumPages()):
+                    page = pdf_reader.getPage(page_num)
+                    text = page.extractText()
+                    results[page_num] = text
+
+            else:
+                print(f"Failed to fetch PDF from URL. Status code: {response.status_code}")
+
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+            results
+
         return results
+
+        # document_analysis_client = DocumentAnalysisClient(
+        #     endpoint=self.form_recognizer_endpoint, credential=AzureKeyCredential(self.form_recognizer_key)
+        # )
+        
+        # poller = document_analysis_client.begin_analyze_document_from_url(
+        #         "prebuilt-layout", formUrl)
+        # layout = poller.result()
+
+        # results = []
+        # page_result = ''
+        # for p in layout.paragraphs:
+        #     page_number = p.bounding_regions[0].page_number
+        #     output_file_id = int((page_number - 1 ) / self.pages_per_embeddings)
+
+        #     if len(results) < output_file_id + 1:
+        #         results.append('')
+
+        #     if p.role not in self.section_to_exclude:
+        #         results[output_file_id] += f"{p.content}\n"
+
+        # for t in layout.tables:
+        #     page_number = t.bounding_regions[0].page_number
+        #     output_file_id = int((page_number - 1 ) / self.pages_per_embeddings)
+            
+        #     if len(results) < output_file_id + 1:
+        #         results.append('')
+        #     previous_cell_row=0
+        #     rowcontent='| '
+        #     tablecontent = ''
+        #     for c in t.cells:
+        #         if c.row_index == previous_cell_row:
+        #             rowcontent +=  c.content + " | "
+        #         else:
+        #             tablecontent += rowcontent + "\n"
+        #             rowcontent='|'
+        #             rowcontent += c.content + " | "
+        #             previous_cell_row += 1
+        #     results[output_file_id] += f"{tablecontent}|"
+
+        # return results
